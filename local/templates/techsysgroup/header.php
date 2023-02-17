b8c013b8cfc46a665f7b7ed18a0f3a5cb42fa5fc
mymya <mymyasn@gmail.com>
2023-02-17 19:45:48 +0500

    Серия 2: как устроен 1C-Битрикс, подключение к сайту через phpstorm

---
+++ local/templates/techsysgroup/header.php
@@ -0,0 +1,27 @@
+<? if (!defined( constant_name: "B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+$CurDir = $APPLICATION->GetCurDir();
+$CurUri = $APPLICATION->GetCurUri();
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <?
+    use Bitrix\Main\Page\Asset;
+    // Пример подключения JS
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/js/jquery-3.6.0.min.js');
+    // Пример подключения CSS
+    Asset::getInstance()->addCSs(SITE_TEMPLATE_PATH . '/css/bootstrap-grid.min.css');
+    $APPLICATION->ShowHead();
+    ?>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title><? $APPLICATION->ShowTitle() ?></title>
+</head>
+<body>
+
+<?
+$APPLICATION->ShowPanel();
+?>
+
+
+</body>
+</html>
